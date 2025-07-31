7ae74e3273ce28c281199d1f494771f7ad71c3b5
Toupak <vitello.th@gmail.com>
2025-07-31 19:13:44 +0200

    Entity Spawn + SFXManager + Entity Cleaner

--- Assets/Entities/EntitySpawner.cs
+++ Assets/Entities/EntitySpawner.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EntitySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] List<GameObject> entityPrefabs;
+    [SerializeField] List<Transform> spawnPositions;
+
+    [SerializeField] private float spawnInterval;
+    
     void Start()
     {
-        
+        //Listener sur le changement de biôme -> On va chercher la liste d'entités correspondante
+        //Listener sur le nombre de boucle/distance parcourue -> On va updater l'intervalle de Spawn / peut-être liste d'entités
+
+        StartCoroutine(SpawnCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnCoroutine()
     {
-        
+        while (true)
+        {
+            Spawn();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private void Spawn()
+    {
+        int whichSpawnPosition = Random.Range(0, spawnPositions.Count);
+        int whichEntity = Random.Range(0, entityPrefabs.Count);
+
+        GameObject entityTemp = Instantiate(entityPrefabs[whichEntity], spawnPositions[whichSpawnPosition].position, Quaternion.identity);
     }
 }
---
+++ Assets/SoundManager/SFXManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSourcePrefab;
+
+    public static SFXManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public AudioSource PlayRandomSFXAtLocation(AudioClip[] clips, Transform target, float volume = 0.1f, float delay = 0.0f, bool loop = false, float pitch = 1f)
+    {
+        int index = Random.Range(0, clips.Length);
+
+        return PlaySFXAtLocation(clips[index], target, volume, delay, loop, pitch);
+    }
+
+    public AudioSource PlaySFXAtLocation(AudioClip clip, Transform target, float volume = 0.1f, float delay = 0.0f, bool loop = false, float pitch = 1f)
+    {
+        Transform parent = target != null ? target : transform;
+
+        AudioSource source = Instantiate(audioSourcePrefab, parent.position, Quaternion.identity, parent);
+
+        source.clip = clip;
+        source.volume = volume;
+        source.loop = loop;
+        source.pitch = pitch + Random.Range(-0.05f, 0.05f);
+        if (delay <= 0.0f)
+            source.Play();
+        else
+            source.PlayDelayed(delay);
+
+        if (!loop)
+            Destroy(source.gameObject, clip.length + delay);
+
+        return source;
+    }
+
+    public AudioSource PlaySFX(AudioClip clip, float volume = 0.1f, float delay = 0.0f, bool loop = false, float pitch = 1f)
+    {
+        return PlaySFXAtLocation(clip, null, volume, delay, loop, pitch);
+    }
+    public AudioSource PlaySFXAtLocationNoPitchModifier(AudioClip clip, Transform target, float volume = 0.1f, float delay = 0.0f, bool loop = false)
+    {
+        Transform parent = target != null ? target : transform;
+
+        AudioSource source = Instantiate(audioSourcePrefab, parent.position, Quaternion.identity, parent);
+
+        source.clip = clip;
+        source.volume = volume;
+        source.loop = loop;
+        if (delay <= 0.0f)
+            source.Play();
+        else
+            source.PlayDelayed(delay);
+
+        if (!loop)
+            Destroy(source.gameObject, clip.length + delay);
+
+        return source;
+    }
+    public AudioSource PlaySFXNoPitchModifier(AudioClip clip, float volume = 0.1f, float delay = 0.0f, bool loop = false)
+    {
+        return PlaySFXAtLocationNoPitchModifier(clip, null, volume, delay, loop);
+    }
+}
