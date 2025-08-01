eedfbcbaeae911df735316093cf9daec54ce6c09
richarx <theorichard12@gmail.com>
2025-08-02 01:25:10 +0200

    fixed blood vfx prefabs

---
+++ Assets/Tools/DestroyAfterDuration.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DestroyAfterDuration : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    
+    private void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
+    public void SetDuration(float newDuration)
+    {
+        duration = newDuration;
+    }
+
+    private void OnDisable()
+    {
+        Destroy(gameObject);
+    }
+}
