f3b662d74a09006a055550e2b64b2fb9dd4c367b
richarx <theorichard12@gmail.com>
2025-08-01 19:18:25 +0200

    thats a push

---
+++ Assets/Parallax/BiomeSwapper.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Parallax
+{
+    public class BiomeSwapper : MonoBehaviour
+    {
+        [SerializeField] private Sprite forest;
+        [SerializeField] private Sprite desert;
+        [SerializeField] private Sprite city;
+    }
+}
