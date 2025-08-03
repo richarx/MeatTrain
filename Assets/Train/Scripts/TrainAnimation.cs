2c26f6ec8982de0701c4f63c4c182018478e4851
Toupak <vitello.th@gmail.com>
2025-08-03 05:45:42 +0200

    Updated prefabs

--- Assets/Train/Scripts/TrainAnimation.cs
+++ Assets/Train/Scripts/TrainAnimation.cs
@@ -1,18 +1,40 @@
+using Locomotor;
 using System.Collections;
 using System.Collections.Generic;
+using Tools;
 using UnityEngine;
 
 public class TrainAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<Transform> trainPieces;
+    [SerializeField] private float maxIntensity;
+
     void Start()
     {
-        
+        for (int i = 0; i < trainPieces.Count; i++)
+        {
+            StartCoroutine(Shake(trainPieces[i]));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Shake(Transform target)
     {
-        
+        while (true)
+        {
+            float intensity = Tools.Tools.NormalizeValueInRange(GreatLocomotor.instance.CurrentSpeed, 0, 2, 0, maxIntensity);
+            
+            Vector2 previousShake = Vector2.zero;
+            float timer = 0.0f;
+
+            Vector2 direction = Random.insideUnitCircle;
+
+            target.position += ((direction * intensity) - previousShake).ToVector3();
+            previousShake = direction * intensity;
+
+            yield return null;
+            timer += Time.deltaTime;
+
+            target.position -= previousShake.ToVector3();
+        }
     }
 }
